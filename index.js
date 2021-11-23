0184a818c6f835fd0d314e44d8baa2d7e599a6fa
Léo Generet <lpgeneret@gmail.com>
2021-11-23 10:12:10 +0100

    Code explication + remove express package

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const cron = require("node-cron");
-const express = require("express");
-const axios = require("axios").default;
-const chalk = require("chalk");
+const cron = require("node-cron"); // permet d'effectuer une fonction à intervalle régulier
+const axios = require("axios").default; // permet de récuperer la data qu'on veut extraire
+const chalk = require("chalk"); // permet de colorer la console
 
-app = express();
+// Toutes les 10 sec, cette fonction est exécutée
 cron.schedule("*/10 * * * * *", function () {
+  
+  // URL trouvée depuis la DevTools Chrome en analysant les requêtes réseau
   var url =
     "https://mobilijeune.actionlogement.fr/api/dossiers/1.0/checkDailyQuota";
 
+  // Stockage de date+heure dans une variable
   var DateTime = new Date();
+  
+  // stockage de la fonction permettant de récuperer la data
   const getData = axios.get(url).then((response) => {
+    
+    // si la fonction renvoie false
     if (response.data === false)
+      // écrire dans la console l'heure et 'FALSE' en rouge
       console.log(
         chalk.bgRed(
           `[${DateTime.getHours()}:${DateTime.getMinutes()}:${DateTime.getSeconds()}] ${
@@ -18,7 +25,10 @@ cron.schedule("*/10 * * * * *", function () {
           }`
         )
       );
+    
+    // sinon
     else
+      // écrire dans la console l'heure et 'TRUE' en vert
       console.log(
         chalk.bgGreen(
           `[${DateTime.getHours()}:${DateTime.getMinutes()}:${DateTime.getSeconds()}] ${
@@ -27,7 +37,7 @@ cron.schedule("*/10 * * * * *", function () {
         )
       );
   });
-
+  
+  // lancer la recupération de la data
   axios.all([getData]);
 });
-app.listen(6666);
